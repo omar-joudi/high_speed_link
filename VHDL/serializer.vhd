ed66cb6b2ae56dd9300a69128686bff1cb28260b
omar-joudi <s-omarmonzer@zewailcity.edu.eg>
2020-04-02 23:30:11 +0200

    Update serializer.vhd

--- VHDL/serializer.vhd
+++ VHDL/serializer.vhd
@@ -16,58 +16,43 @@ use IEEE.numeric_std.ALL;
 
 entity serializer is
 	port (
-		bit_clk  : in std_logic; 
-		data_in  : in std_logic_vector(9 downto 0);
-		data_out : out std_logic
+	   clk      : in std_logic;
+	   bit_clk  : in std_logic; 
+	   data_in  : in std_logic_vector(9 downto 0); 
+	   data_out : out std_logic
 	);
 end serializer;
 
 
 architecture arch of serializer is
 
-	signal count_r : integer range 0 to 10;
-	signal count_f : integer range 0 to 10;
+	signal input : std_logic_vector(9 downto 0);
 	
-	signal op_r :std_logic;
-	signal op_f :std_logic;
+	signal op_r : std_logic;
+	signal op_f : std_logic;
+	 
+	signal clk_s : std_logic;
 
 begin
 	
 	ser_r : process (bit_clk)
 	begin
 		if rising_edge(bit_clk) then
-			if data_in = "0000000000" then
-				count_r <= 10;
-			else
-				if count_f = 10 then 
-					count_r <= 0;
-				else
-					count_r <= count_f + 1;
-				end if;
-			end if;
-		
-			if count_f /= 10 then 
-				op_r <= data_in(count_f);
-			end if;
+		    if clk_s = clk then
+		        op_r  <= input(0);
+		        input <= input(1) & '0' & input(9 downto 2);
+		    else
+		        op_r  <= data_in(0);
+		        input <= data_in(1) & '0' & data_in(9 downto 2);
+		        clk_s <= clk;
+		    end if;
 		end if;
 	end process ser_r;
 	
 	ser_f : process (bit_clk)
 	begin
 		if falling_edge(bit_clk) then
-			if data_in = "0000000000" then
-				count_f <= 10;
-			else
-				if count_r = 10 then 
-					count_f <= 0;
-				else
-					count_f <= count_r + 1;
-				end if;
-			end if;
-		
-			if count_r /= 10 then 
-				op_f <= data_in(count_r);
-			end if;
+		    op_f <= input(9);
 		end if;
 	end process ser_f;
 	
