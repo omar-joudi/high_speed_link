a3ce719db5ed16be6c74f1ef2971362d417cd8b3
omar-joudi <s-omarmonzer@zewailcity.edu.eg>
2020-04-02 23:33:16 +0200

    Update deserializer.vhd

--- VHDL/deserializer.vhd
+++ VHDL/deserializer.vhd
@@ -19,6 +19,7 @@ entity deserializer is
 		link : std_logic_vector(9 downto 0) := "0011111000"
 	);
 	port (
+		clk      : in std_logic;
 		bit_clk  : in std_logic; 
 		data_in  : in std_logic;
 		data_out : out std_logic_vector(9 downto 0)
@@ -28,56 +29,37 @@ end deserializer;
 
 architecture arch of deserializer is
 
-	signal reg_r   : std_logic_vector(9 downto 0);
-	signal reg_f   : std_logic_vector(9 downto 0);
+	signal clk_s : std_logic;
 	
-	signal op_r   : std_logic_vector(9 downto 0);
-	signal op_f   : std_logic_vector(9 downto 0);
-	
-	signal count_r : integer range 0 to 10;
-	signal count_f : integer range 0 to 10;
+	signal op_r : std_logic_vector(4 downto 0);
+	signal op_f : std_logic_vector(4 downto 0);
 
 begin
 
 	deser_r : process (bit_clk)
 	begin	
-		if rising_edge(bit_clk)  then
-			reg_r <= data_in & reg_f(9 downto 1);
-			
-			if reg_f = link then 
-				count_r <= 0;
-			else 
-				if count_f = 10 then 
-					count_r <= 0;
-					op_r    <= reg_f;
-				else 
-					count_r <= count_f + 1;
-				end if;
-			end if;
-			
+		if rising_edge(bit_clk) then
+		    op_r <= data_in & op_r(4 downto 1);
 		end if;	
 	end process deser_r;
 	
 	deser_f : process (bit_clk)
 	begin	
 		if falling_edge(bit_clk)  then
-			reg_f <= data_in & reg_r(9 downto 1);
-			
-			if reg_r = link then 
-				count_f <= 0;
-			else 
-				if count_r = 10 then 
-					count_f <= 0;
-					op_f    <= reg_r;
-				else 
-					count_f <= count_r + 1;
-				end if;
-			end if;
-			
+		    op_f <= data_in & op_f(4 downto 1);
 		end if;	
 	end process deser_f;
 	
-	data_out <= op_f when (count_r = 10 and count_f = 0) or count_r = 1 or count_r = 3 or count_r = 5 or count_r = 7 or count_r = 9 else
-	            op_r;
+	process (bit_clk)
+	begin
+	    if falling_edge(bit_clk) then
+		    if clk_s /= clk then
+		        clk_s <= clk;
+		        data_out <= op_r(4) & op_f(4) & op_r(3) & op_f(3) & 
+				    op_r(2) & op_f(2) & op_r(1) & op_f(1) & 
+				    op_r(0) & op_f(0);
+		    end if;
+		end if;
+    end process;
 	
 end arch;
